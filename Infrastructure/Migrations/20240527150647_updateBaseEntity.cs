5e071fca6fc3dc420c80e4b78cdfbe15bb435430
maura2112 <championlc97@gmail.com>
2024-05-27 22:07:39 +0700

    update baseentity
    add migration

---
+++ Infrastructure/Migrations/20240527150647_updateBaseEntity.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserSkills",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Stages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Skills",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ProjectStatus",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ProjectSkills",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Projects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "MediaFolder",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Bookmarks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "BidStages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Address",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserSkills");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Stages");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Skills");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ProjectStatus");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ProjectSkills");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "MediaFolder");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Bookmarks");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "BidStages");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Address");
+        }
+    }
+}

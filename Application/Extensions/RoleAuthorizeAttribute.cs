0ece4df48d827b4b484295805b224662ebbf4eba
Bui Quang Truong <championlc97@gmail.com>
2024-07-20 15:52:08 +0700

    Update RoleAuthorizeAttribute.cs

--- Application/Extensions/RoleAuthorizeAttribute.cs
+++ Application/Extensions/RoleAuthorizeAttribute.cs
@@ -11,17 +11,34 @@ namespace Application.Extensions
 {
     public class RoleAuthorizeAttribute : Attribute, IAuthorizationFilter
     {
-        private readonly string _role;
+        private readonly string[] _roles;
 
-        public RoleAuthorizeAttribute(string role)
+        public RoleAuthorizeAttribute(params string[] roles)
         {
-            _role = role;
+            _roles = roles ?? throw new ArgumentNullException(nameof(roles));
         }
+
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            var currentUserService = (ICurrentUserService) context.HttpContext.RequestServices.GetService(typeof(ICurrentUserService));
+            var currentUserService = (ICurrentUserService)context.HttpContext.RequestServices.GetService(typeof(ICurrentUserService));
 
-            if (currentUserService == null || !currentUserService.HasRole(_role))
+            if (currentUserService == null)
+            {
+                context.Result = new ForbidResult();
+                return;
+            }
+
+            bool isAuthorized = false;
+            foreach (var role in _roles)
+            {
+                if (currentUserService.HasRole(role))
+                {
+                    isAuthorized = true;
+                    break;
+                }
+            }
+
+            if (!isAuthorized)
             {
                 context.Result = new ForbidResult();
             }
